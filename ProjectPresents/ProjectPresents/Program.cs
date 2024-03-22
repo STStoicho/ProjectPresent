50c8e789806a10025fffac51fe05c2b54e6f33fe
STStoicho <stoichotrifonov1@gmail.com>
2024-03-22 18:17:19 +0200

    Added roles and change views

--- ProjectPresents/ProjectPresents/Program.cs
+++ ProjectPresents/ProjectPresents/Program.cs
@@ -11,7 +11,6 @@ namespace ProjectPresents
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
@@ -33,7 +32,6 @@ namespace ProjectPresents
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
